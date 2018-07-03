41e80aa716246c16a452b4aa8126ae35dbe0dd63
shaban <mohamed19936@gmail.com>
2018-07-03 19:43:51 +0200

    adding test file

--- src/test.py
+++ src/test.py
@@ -10,77 +10,18 @@ from src.forecaster.XGBForecaster import XGBForecaster
 from src.data.feedforward_data import Data
 
 
-def test1():
-    iris = datasets.load_breast_cancer()
-    X = iris.data
-    y = iris.target
-    print(y)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # use DMatrix for xgbosot
-    dtrain = xgb.DMatrix(X_train, label=y_train)
-    dtest = xgb.DMatrix(X_test, label=y_test)
-
-    # use svmlight file for xgboost
-    dump_svmlight_file(X_train, y_train, 'dtrain.svm', zero_based=True)
-    dump_svmlight_file(X_test, y_test, 'dtest.svm', zero_based=True)
-    dtrain_svm = xgb.DMatrix('dtrain.svm')
-    dtest_svm = xgb.DMatrix('dtest.svm')
-
-    # set xgboost params
-    param = {
-        'max_depth': 3,  # the maximum depth of each tree
-        'eta': 0.3,  # the training step for each iteration
-        'silent': 1,  # logging mode - quiet
-        'objective': 'multi:softprob',  # error evaluation for multiclass training
-        'num_class': 3}  # the number of classes that exist in this datset
-    num_round = 20  # the number of training iterations
-
-    # ------------- numpy array ------------------
-    # training and testing - numpy matrices
-
-    bst = xgb.train(param, dtrain, num_round)
-    print(type(bst))
-    preds = bst.predict(dtest)
-
-    # extracting most confident predictions
-    best_preds = np.asarray([np.argmax(line) for line in preds])
-    print("Numpy array precision:", precision_score(y_test, best_preds, average='macro'))
-
-    # ------------- svm file ---------------------
-    # training and testing - svm file
-    bst_svm = xgb.train(param, dtrain_svm, num_round)
-    preds = bst.predict(dtest_svm)
-
-    # extracting most confident predictions
-    best_preds_svm = [np.argmax(line) for line in preds]
-    print("Svm file precision:", precision_score(y_test, best_preds_svm, average='macro'))
-    # --------------------------------------------
-
-    # dump the models
-    bst.dump_model('dump.raw.txt')
-    bst_svm.dump_model('dump_svm.raw.txt')
-
-    # save the models for later
-    joblib.dump(bst, 'bst_model.pkl', compress=True)
-    joblib.dump(bst_svm, 'bst_svm_model.pkl', compress=True)
-
-
 def test2():
-    data = Data.load_data()
-    # print(len(data.y_test))
-    # data.save()
-    # exit(0)
-    # params = {'reg_alpha': 0.8407902514954478, 'max_depth': 14, 'learning_rate': 0.08579491614428596,
-    #           'gamma': 0.7967508936551793, **XGBForecaster.initial_params}
-    forecaster = XGBForecaster.load_model("XGBForecaster2018-07-03-16:01")
-
-    # print(len(data.X_val) + len(data.X_test))
-    # data.X_val, X_test, data.y_val, y_test = train_test_split(data.X_val, data.y_val, test_size=0.2, random_state=35)
-    # print(len(data.X_val))
-    # exit(0)
-    # forecaster.fit(data)
-    print("validating")
+    try:
+        data = Data.load_data()
+    except:
+        data = Data()
+        data.save()
+    try:
+        forecaster = XGBForecaster.load_model("XGBForecaster2018-07-03-16:01")
+    except:
+        forecaster = XGBForecaster()
+        forecaster.fit(data)
+    print("validating model")
     forecaster.score(data)
 
 
