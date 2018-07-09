035188c23c1945afa3871697e580cbb345214064
Kübra Seyhan <kubra.seyhan@campus.tu-berlin.de>
2018-07-09 20:30:49 +0200

    modelperformance

--- src/model_performance.py
+++ src/model_performance.py
@@ -26,7 +26,6 @@ def rmspe_xg(yhat, y):
     rmspe = np.sqrt(np.mean(w * (y - yhat) ** 2))
     return "rmspe", rmspe
 
-# Gather some features
 def build_features(features, data):
     # remove NaNs
     data.fillna(0, inplace=True)
@@ -72,55 +71,55 @@ def build_features(features, data):
     data['assortment'] = data['assortment'].astype(float)
 
 
-print("Load the training, test and store data using pandas")
-train = pd.read_csv("../input/train.csv")
-test = pd.read_csv("../input/test.csv")
-store = pd.read_csv("../input/store.csv")
+    print("Load the training, test and store data using pandas")
+    train = pd.read_csv("../input/train.csv")
+    test = pd.read_csv("../input/test.csv")
+    store = pd.read_csv("../input/store.csv")
 
-print("Assume store open, if not provided")
-test.fillna(1, inplace=True)
+    print("Assume store open, if not provided")
+    test.fillna(1, inplace=True)
 
-print("Consider only open stores for training. Closed stores wont count into the score.")
-train = train[train["Open"] != 0]
+    print("Consider only open stores for training. Closed stores wont count into the score.")
+    train = train[train["Open"] != 0]
 
-print("Join with store")
-train = pd.merge(train, store, on='Store')
-test = pd.merge(test, store, on='Store')
+    print("Join with store")
+    train = pd.merge(train, store, on='Store')
+    test = pd.merge(test, store, on='Store')
 
-features = []
+    features = []
 
-print("augment features")
-build_features(features, train)
-build_features([], test)
-print(features)
+    print("augment features")
+    build_features(features, train)
+    build_features([], test)
+    print(features)
 
-params = {"objective": "reg:linear",
+    params = {"objective": "reg:linear",
           "eta": 0.2,
           "max_depth": 8,
           "subsample": 0.7,
           "colsample_bytree": 0.7,
           "silent": 1
           }
-num_trees = 900
+    num_trees = 900
 
-print("Train a XGBoost model")
-X_train, X_test = cross_validation.train_test_split(train, test_size=0.0125)
-dtrain = xgb.DMatrix(X_train[features], np.log(X_train["Sales"] + 1))
-dvalid = xgb.DMatrix(X_test[features], np.log(X_test["Sales"] + 1))
-dtest = xgb.DMatrix(test[features])
-watchlist = [(dvalid, 'eval'), (dtrain, 'train')]
-gbm = xgb.train(params, dtrain, num_trees, evals=watchlist, early_stopping_rounds=50, feval=rmspe_xg, verbose_eval=True)
+    print("Train a XGBoost model")
+    X_train, X_test = cross_validation.train_test_split(train, test_size=0.0125)
+    dtrain = xgb.DMatrix(X_train[features], np.log(X_train["Sales"] + 1))
+    dvalid = xgb.DMatrix(X_test[features], np.log(X_test["Sales"] + 1))
+    dtest = xgb.DMatrix(test[features])
+    watchlist = [(dvalid, 'eval'), (dtrain, 'train')]
+    gbm = xgb.train(params, dtrain, num_trees, evals=watchlist, early_stopping_rounds=50, feval=rmspe_xg, verbose_eval=True)
 
-print("Validating")
-train_probs = gbm.predict(xgb.DMatrix(X_test[features]))
-indices = train_probs < 0
-train_probs[indices] = 0
-error = rmspe(np.exp(train_probs) - 1, X_test['Sales'].values)
-print('error', error)
+    print("Validating")
+    train_probs = gbm.predict(xgb.DMatrix(X_test[features]))
+    indices = train_probs < 0
+    train_probs[indices] = 0
+    error = rmspe(np.exp(train_probs) - 1, X_test['Sales'].values)
+    print('error', error)
 
-print("Make predictions on the test set")
-test_probs = gbm.predict(xgb.DMatrix(test[features]))
-indices = test_probs < 0
-test_probs[indices] = 0
-submission = pd.DataFrame({"Id": test["Id"], "Sales": np.exp(test_probs) - 1})
-submission.to_csv("xgboost_kscript_submission.csv", index=False)
+    print("Make predictions on the test set")
+    test_probs = gbm.predict(xgb.DMatrix(test[features]))
+    indices = test_probs < 0
+    test_probs[indices] = 0
+    submission = pd.DataFrame({"Id": test["Id"], "Sales": np.exp(test_probs) - 1})
+    submission.to_csv("xgboost_kscript_submission.csv", index=False)
