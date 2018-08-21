27b1a0b5829b1f64a2d60a402ac4774d86a80577
Niels Warncke <niels.warncke@gmail.com>
2018-08-22 01:12:41 +0200

    data caches extracted data for stores, doesn't have to execute at once

--- src/data/default_data.py
+++ src/data/default_data.py
@@ -13,7 +13,8 @@ BATCH_SIZE = 50
 
 class Data():
     def __init__(self, update_disk=False):
-        self.batches_X, self.batches_y = None, None
+        self.batches_X, self.batches_y, self.store_ids, self.store_ids = None, None, None, None
+        # self.store_ids is to look up the store_id of a batch, has shape (#batches)
         try:
             assert not update_disk
             self.load(os.path.join(DATA_DIR, "extracted"))
@@ -26,38 +27,80 @@ class Data():
         self.train_batch_ids = set()
         self.test_batch_ids = set()
 
-    def extract(self):
-        data_extract = DataExtraction()
+    def cached_batches_of(self, store_id):
+        cached_dir = os.path.join(DATA_DIR, "extracted")
+        all_X = np.load(os.path.join(cached_dir, "store_{}_X.npy".format(store_id)))
+        all_y = np.load(os.path.join(cached_dir, "store_{}_y.npy".format(store_id)))
+        num_samples = all_X.shape[0]
+        num_samples -= num_samples % BATCH_SIZE
+        num_batches = int(num_samples/BATCH_SIZE)
+        all_X = all_X[:num_samples]
+        all_y = all_y[:num_samples]
+        batches_X = np.reshape(all_X, [num_batches, BATCH_SIZE]+list(all_X.shape[1:]))
+        batches_y = np.reshape(all_y, [num_batches, BATCH_SIZE]+list(all_y.shape[1:]))
+        print("... loaded cached data for store", store_id)
+        return batches_X, batches_y
+
+    def load_batches_of(self, store_id):
+        try:
+            return self.cached_batches_of(store_id)
+        except FileNotFoundError as e:
+            print("... generate data for store", store_id)
 
         # generate all batches
-        df = data_extract.train
+        df = self.data_extract.train
         store_ids = df.Store.unique()
 
-        self.batches_X, self.batches_y = [], []
-        for store_id in store_ids:
-            row_ids = df.index[df.Store==store_id]
-            days, X, y = data_extract.extract_rows_and_days(row_ids)
-            days, X, y = days[::-1], X[::-1], y[::-1]
+        row_ids = df.index[df.Store == store_id]
+        days, X, y = self.data_extract.extract_rows_and_days(row_ids)
+        days, X, y = days[::-1], X[::-1], y[::-1]
 
-            start = 0
-            while start < days[-1]-BATCH_SIZE:
-                end = min(start + BATCH_SIZE, len(days))
+        start = 0
+        batches_X, batches_y = [], []
+        while start < len(days) - BATCH_SIZE:
+            end = min(start + BATCH_SIZE, len(days))
 
-                # check if dates are missing and set end to where it's missing
-                if np.all(np.array(days[1:]) - np.array(days[:-1]) != np.ones(len(days)-1)):
-                    end = np.where(np.array(days[1:]) - np.array(days[:-1]) != np.ones(len(days)-1))[0][0]
+            # check if dates are missing and set end to where it's missing
+            if np.all(np.array(days[1:]) - np.array(days[:-1]) != np.ones(len(days) - 1)):
+                end = np.where(np.array(days[1:]) - np.array(days[:-1]) != np.ones(len(days) - 1))[0][0]
 
-                self.batches_X.append(X[start:end])
-                self.batches_y.append(y[start:end])
-                start = end
+            batches_X.append(X[end-BATCH_SIZE:end])
+            batches_y.append(y[end-BATCH_SIZE:end])
+            start = end
 
-        self.batches_X, self.batches_y = np.array(self.batches_X), np.array(self.batches_y)
+        # save batches
+        cached_dir = os.path.join(DATA_DIR, "extracted")
+        os.makedirs(cached_dir, exist_ok=True)
+        batches_X, batches_y = np.array(batches_X), np.array(batches_y)
+        all_X = np.reshape(batches_X, [batches_X.shape[0]*batches_X.shape[1]]+list(batches_X.shape[2:]))
+        all_y = np.reshape(batches_y, [batches_y.shape[0]*batches_y.shape[1]]+list(batches_y.shape[2:]))
+        np.save(os.path.join(cached_dir, "store_{}_X".format(store_id)), all_X)
+        np.save(os.path.join(cached_dir, "store_{}_y".format(store_id)), all_y)
+
+        return batches_X, batches_y
+
+    def extract(self):
+        self.data_extract = DataExtraction()
+        # generate all batches
+        df = self.data_extract.train
+        self.store_ids = list(df.Store.unique())
+        self.store = []
+
+        # for each batch, the store can be checked in self.store (of len: #batches)
+        for store_id in self.store_ids:
+            batches_X, batches_y = self.load_batches_of(store_id)
+            self.store += [store_id]*len(batches_X)
+            if self.batches_X is not None:
+                self.batches_X = np.concatenate([self.batches_X, batches_X], 0)
+                self.batches_y = np.concatenate([self.batches_y, batches_y], 0)
+            else:
+                self.batches_X, self.batches_y = batches_X, batches_y
+        self.store = np.array(store)
         self.save(os.path.join(DATA_DIR, "extracted"))
 
     def all_train_data(self):
         batches = list(self.train_batch_ids)
         return np.concatenate(self.batches_X[batches], axis=0), np.concatenate(self.batches_y[batches], axis=0)
-
 
     def all_test_data(self):
         batches = list(self.test_batch_ids)
@@ -68,10 +111,12 @@ class Data():
         os.makedirs(path, exist_ok=True)
         np.save(os.path.join(path, "X"), self.batches_X)
         np.save(os.path.join(path, "y"), self.batches_y)
+        np.save(os.path.join(path, "store_ids"), self.store_ids)
 
     def load(self, path):
-        self.batches_X = np.load(os.path.join(path, "X"))
-        self.batches_y = np.load(os.path.join(path, "y"))
+        self.batches_X = np.load(os.path.join(path, "X.npy"))
+        self.batches_y = np.load(os.path.join(path, "y.npy"))
+        self.store_ids = np.load(os.path.join(path, "store_ids.npy"))
 
     def new_epoch(self):
         self.epochs += 1
--- src/model_selection.py
+++ src/model_selection.py
@@ -1,10 +1,17 @@
 from data import Data
 import numpy as np
+import json
+import os
 try:
     from .forecaster import *
 except:
     from forecaster import *
 import pdb
+
+MODELS = [NaiveForecaster, XGBForecaster, LinearRegressor, FeedForwardNN1]
+RESULT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../model_selection_results")
+
+os.makedirs(RESULT_DIR, exist_ok=True)
 
 def estimate_score(model, data):
     # use the first 1000 batches only
@@ -19,9 +26,57 @@ def estimate_score(model, data):
         score.append(model.score(data.batch_X[batch_id], data.batch_y[batch_id]))
     return np.mean(score)
 
+
 def test_estimate_score():
     model = NaiveForecaster()
-    print("Score: ", estimate_score(model, Data(update_disk=True)))
+    data = Data(update_disk=True)
+    print("Got some data")
+    print("Score: ", estimate_score(model, data))
+
+
+def model_selection():
+    """
+    Find the best model, their hyperparameters and estiamted score
+    generate a model_score.json that looks like this:
+    { <class_name>: {"hyper_parameters": <hyperparameters>, "score": <score>}, ...}
+    :return the best untrained model:
+    """
+    result_path = os.path.join(RESULT_DIR, 'model_selection.json')
+    try: # load old results and don't run modelselection for them again
+        with open(result_path, "r") as f:
+            result = json.load(f)
+    except FileNotFoundError:
+        result = {} # here the json results will be collected
+
+    for model_class in MODELS:
+        if model_class in result.keys(): continue
+        params, score = best_hyperparams_and_score(model_class)
+        result[model_class.__name__] = {"hyper_parameters": params, "score": score}
+
+    #  save the results
+    with open(result_path, "w") as f:
+        result = json.dumps(result)
+        print("result to be dumped", result)     
+        f.write(result)
+
+
+
+def train_best_model():
+    model = model_selection()
+    data = Data()
+    model.fit(data)
+    print("Save the best trained model")
+    try:
+        path = model.save()
+        print("Saved to ", path)
+        with open("best_model.txt", "w") as file:
+            file.write(path)
+    except Exception as e:
+        print("While saving the best model, an error has occurred")
+        print(type(e))
+        print(e)
+        pdb.set_trace()
+
 
 def main():
     test_estimate_score()
